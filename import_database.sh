3f30ee3702cc98c5b2f64a0175a63ca5a1e5240e
Sooknu <sahid@live.com>
2025-03-24 17:09:06 -0700

    drops databases before importing

--- import_database.sh
+++ import_database.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
 # Database credentials
 DB_USER="user"
 DB_PASSWORD="password"
+DB_NAME="your_database_name" # You should replace this with your actual database name
 
 # Database dump file
 DB_DUMP_FILE="/path/to/your/backup_dump.sql"
 
-# Import the databases
-if mysql -u"$DB_USER" -p"$DB_PASSWORD" < "$DB_DUMP_FILE"; then
+# Drop existing and create new database
+mysql -u"$DB_USER" -p"$DB_PASSWORD" -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;"
+
+# Import the database
+if mysql -u"$DB_USER" -p"$DB_PASSWORD" $DB_NAME < "$DB_DUMP_FILE"; then
     echo "$(date) - Database import successful."
 else
     echo "$(date) - ERROR: Database import failed!"
